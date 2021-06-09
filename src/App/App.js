86e02404b6125093399f011d963e1eda18f375d1
Jordon Smith <jordon00smith@gmail.com>
2021-06-08 20:27:08 -0500

    Initial page setup

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super()
+
+  }
+
+  render() {
+    return(
+      <section classname='app'>
+        <h1 className='page-title'>GitHub Repository Search Engine</h1>
+      </section>
+    )
+  }
+}
+
+export default App;
