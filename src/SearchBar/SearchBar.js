60a18b2f2019f0c0e4713618348d39e2e9d3acd6
Jordon Smith <jordon00smith@gmail.com>
2021-06-09 14:58:22 -0500

    Update spacing for search area

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -17,7 +17,7 @@ class SearchBar extends Component {
 
   render() {
     return(
-      <section>
+      <section className='search-section'>
         <form className='search-bar-form'>
           <input className='search-bar-input'
             type='text'
@@ -29,25 +29,29 @@ class SearchBar extends Component {
           <button className='search-bar-button' onClick={ event => this.props.filterRepos(event, this.state.repoName) }>
             SEARCH
           </button>
-          <button className='sort-by-stars-button' onClick={ event => this.props.sortReposByStars(event, this.state.repoName) }>
-            Sort By Stars
-          </button>
-          <button className='sort-by-match-button' onClick={ event => this.props.filterRepos(event, this.state.repoName) }>
-            Sort By Best Match
-          </button>
         </form>
-        <form>
-          <input className='language-input'
-            type='text'
-            placeholder='Filter by language...'
-            name='language'
-            value={ this.state.language }
-            onChange={ event => this.handleInput(event) }
-          />
-          <button className='language-filter-btn' onClick={ event => this.props.filterByLanguage(event, this.state.repoName, this.state.language) }>
-            Filter
-          </button>
-        </form>
+        <section className='sort-and-filter-section'>
+          <div className='sort-buttons-section'>
+            <button className='sort-by-stars-button' onClick={ event => this.props.sortReposByStars(event, this.state.repoName) }>
+              Sort By Stars
+            </button>
+            <button className='sort-by-match-button' onClick={ event => this.props.filterRepos(event, this.state.repoName) }>
+              Sort By Best Match
+            </button>
+          </div>
+          <form className='language-filter-form'>
+            <input className='language-input'
+              type='text'
+              placeholder='Filter by language...'
+              name='language'
+              value={ this.state.language }
+              onChange={ event => this.handleInput(event) }
+            />
+            <button className='language-filter-button' onClick={ event => this.props.filterByLanguage(event, this.state.repoName, this.state.language) }>
+              Filter
+            </button>
+          </form>
+        </section>
       </section>
     )
   }
